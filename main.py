b2e11666069c65cd1491c626f68952839b576e18
fantasy <81114713+fantasywastaken@users.noreply.github.com>
2024-01-29 04:40:32 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+import requests
+
+def check_domain(domain):
+    headers = {
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36 Edg/121.0.0.0',
+        'x-requested-with': 'XMLHttpRequest',
+    }
+    data = {
+        'domainname': domain,
+    }
+    response = requests.post('https://www.turkticaret.net/siberhosting/whoisAjax.php', headers=headers, data=data)
+    if response.text.__contains__('whois'):
+        return False
+    return True
+
+domains = [
+    'domain1.com', 'domain2.com', 'domain3.com'
+]
+
+for domain in domains:
+    is_available = check_domain(domain)
+    if is_available:
+        print(f"{domain}: Available")
